df2a2e5b56f4a0eb93caed7bfd72dbc284924bf4
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-23 15:06:32 +0000

    mail ok

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,21 @@
 
 // MAIL
+let mail = prompt('Scrivi il tuo indirizzo mail');
+
 const listaMail = ['pippo@pippo.it', 'mario@rossi.it', 'davide@piarulli.it'];
 
-if()
+let verifica = false;
 
-  let mail = prompt('Scrivi il tuo indirizzo mail');
-  console.log(mail);
+for (let i = 0; i < listaMail.length; i++) {
+  if(mail == listaMail[i]){
+    verifica = true;
+  }
+}
+
+if(verifica){
+  document.getElementById('mailUtente').innerHTML = 'La tua mail è presente in lista';
+} document.getElementById('mailUtente').innerHTML = 'Mi dispiace, non sei presente in lista';
+
 
  
 
