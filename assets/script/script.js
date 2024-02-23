1d6ae3a580657a175e3107bee1b27ecef987ab66
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-23 16:18:39 +0000

    fix

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,36 +1,37 @@
 
 // MAIL
 let mail = prompt('Scrivi il tuo indirizzo mail');
-
 const listaMail = ['pippo@pippo.it', 'mario@rossi.it', 'davide@piarulli.it'];
-
 let verifica = false;
 
-for (let i = 0; i < listaMail.length; i++) {
-  if(mail == listaMail[i]){
-    verifica = true;
+
+  for (let i = 0; i < listaMail.length; i++) {
+    if(mail == listaMail[i]){
+      verifica = true;
+    }
   }
-}
 
-if(verifica){
-  document.getElementById('mailUtente').innerHTML = 'La tua mail è presente in lista';
-} document.getElementById('mailUtente').innerHTML = 'Mi dispiace, non sei presente in lista';
 
+  if(verifica){
+    document.getElementById('mailUtente').innerHTML = 'La tua mail è presente in lista';
+  }else{
+    document.getElementById('mailUtente').innerHTML = 'Mi dispiace, non sei presente in lista';
+  }
 
 
 // DADI
 
 
-document.getElementById('dpbtn').addEventListener("click", function () {
+document.getElementById('dpbtn2').addEventListener("click", function () {
   const numRandomUtente = Math.ceil(Math.random() * 6);
   const numRandomPc = Math.ceil(Math.random() * 6);
 
 
   if (numRandomPc < numRandomUtente) {
-    document.getElementById('output').innerHTML = 'utente vince ';
+    document.getElementById('output').innerHTML = `Utente vince con: ${numRandomUtente} a ${numRandomPc}` ;
   } else if (numRandomPc > numRandomUtente) {
-    document.getElementById('output').innerHTML = 'il PC vince';
+    document.getElementById('output').innerHTML = `il PC vince con: ${numRandomPc} a ${numRandomUtente}`;
   } else {
-    document.getElementById('output').innerHTML = 'Questo è un pareggio';
+    document.getElementById('output').innerHTML = `Questo è un pareggio ${numRandomPc} a ${numRandomUtente}`;
   }
 });
